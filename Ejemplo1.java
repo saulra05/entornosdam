3d0d2e52faa3b70778a2ff80506fa80fde6f170d
dam23-18 <dam23-18@c16x22.iescierva.net>
2024-01-30 09:44:09 +0100

    Añadir gitignore

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,7 +1,8 @@
 public class Ejemplo1{
-    public stataic void main(String[] args){
+    public static void main(String[] args){
         System.out.println("HOLA MUNDO");
         String nombre= "Saúl";
         System.out.print("Mi nombre es " + nombre);
+    }
 }
 
